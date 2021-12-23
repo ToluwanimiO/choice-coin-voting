390037c230e0623aedbcde7f4b8f2374a9d2b6fe
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2021-12-23 01:08:44 +0100

    made changes to design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+// import React, {useState} from "react";
 import './App.css';
 import logo from './assets/logo2.png'
 import welcome from './assets/bgwelcome.jpg'
 import MyAlgoConnect from '@randlabs/myalgo-connect';
 // import algosdk from "algosdk"; 
 const myAlgoWallet = new MyAlgoConnect();
+// const [value, setValue] = useState(1);
 
 async function connectToMyAlgo() {
   // This function connects the voters wallet to my program, it brings up a pop up used to connect user's wallet
@@ -12,17 +14,15 @@ async function connectToMyAlgo() {
     const accounts = await myAlgoWallet.connect();
     const addresses = accounts.map(account => account.address);
     window.acc = accounts
-    // if addresses is not empty, it returns true and displays the voting div and takes away the welcome image
+    // if addresses is not empty, the condition returns true and focuses on the input for choice amount and display user's address
     if (addresses)
-    {
-      var element = document.getElementById("welcome");
-      element.classList.add("d-none");
-      var element2 = document.getElementById("wallet");
-      element2.classList.remove("d-none");
-      var element2 = document.getElementById("success");
-      element2.classList.add("d-none");   
+    {   
       // focuses on the input automatically
       document.getElementById("amount").focus();
+      console.log(accounts[0].name)
+      // this.setState({
+      //   nameUser:accounts[0].name
+      // })
     }
     
   // in case of an error in connection
@@ -35,23 +35,34 @@ function App() {
   // returns what is displayed for the user to see
   return (
     <div className="">
-      <nav className="navbar navbar-light bg-white justify-content-between pl-5 pr-5 shadow-sm">
+      <nav className="navbar justify-content-between pl-5 pr-5 shadow-sm">
         
-        <a href='/' className="navbar-brand d-block d-md-none"><img alt="text" src={logo} width="auto" height="50px"/></a>
-        <a href='/' className="navbar-brand d-none d-md-block"><img alt="text" src={logo} width="auto" height="50px"/> &nbsp;Choice Coin Voting</a>
-        
+        {/* <a href='/' className="navbar-brand d-block d-md-none"><img alt="text" src={logo} width="auto" height="50px"/></a> */}
+        <a href='/' className="navbar-brand d-none d-md-block letter">
+          {/* <img alt="text" src={logo} width="auto" height="50px"/>  */}
+          &nbsp;Choice Coin Voting
+        </a>
+        <a href='/' className="navbar-brand d-block d-md-none letter">
+          {/* <img alt="text" src={logo} width="auto" height="50px"/>  */}
+          &nbsp;Choice
+        </a>
+        {/* <span>{this.state.nameUser}</span> */}
         <button id="connectBtn" className="btn btn-lg btn-outline-primary my-2 my-sm-0" onClick={connectToMyAlgo}>Connect Wallet</button>
         
-    </nav>
-      <div className="card marginFix shadow-sm">
-        <img alt="text" id="welcome" src={welcome} width="100%" height="100%"/>
+      </nav>
+      <div className="dropdown-divider bg-secondary"></div>
+      <div className=" marginFix shadow-sm">
+        {/* <img alt="text" id="welcome" src={welcome} width="100%" height="100%"/> */}
         
         
-        <div id="wallet" className="card-body d-none padIssue p-3">
-            <p>Issue Number: <span style={{fontWeight: "100"}}>AQUJDI4DJH68</span></p>
-            <input type="text" className="form-control lengthen" disabled value="Statement of Issue to be voted on:" />
-            <span>Amount</span>
-            <input id="amount" type="number" className="form-control" placeholder='Input choice Amount'/>
+        <div id="wallet" className="text-white padIssue ">
+            <div className="card color pl-5 pr-5 mb-5 pt-3 pb-5"> 
+              <p>Issue Number: <span style={{fontWeight: "100"}}>AQUJDI4DJH68</span></p>
+              <input type="text" className=" colorborder form-control" disabled value="Statement of Issue to be voted on:" />
+            
+            </div>
+            <span className="mt-5">Amount</span>
+            <input id="amount" type="number" className="form-control colorborder2 mt-2" placeholder='Input choice Amount'/>
             <div className="row text-center mt-3">
                 <div className="col-6">
                     <p>Yes&nbsp;<input type="radio" id="yes" name="choice"/></p>
@@ -65,11 +76,11 @@ function App() {
                 <button onClick={vote} className="btn btn-lg btn-primary btn-color-blue">Submit</button>
             </div>
         </div>
-        <div id="success" className="card-body text-center p-3 d-none" style={{height: "25em"}}>
+        <div id="success" className=" text-center p-3 d-none" style={{height: "25em"}}>
             <div className="text-center centerSuccess">
                 <i className="fa fa-check fa-5x text-success"></i><br/>
                 <p className="text-success "><b>Succesfully Voted </b></p>
-                <i id="result"></i>
+                <i id="result" className="text-white"></i>
             </div>
         </div>
         
